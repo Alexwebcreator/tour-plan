dc35fa71666fa8651bb6b43e59007c38bfea6e83
Alexwebcreator <Sanek72Tob@mail.ru>
2021-09-20 20:35:51 +0500

    create: added switch keyboard on hotel slider

--- js/main.js
+++ js/main.js
@@ -7,5 +7,10 @@ const swiper = new Swiper('.swiper', {
     nextEl: '.slider-button--next',
     prevEl: '.slider-button--prev',
   },
-});
 
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+    pageUpDown: true,
+  },
+});
