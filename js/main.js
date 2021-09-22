8aee3216351e95280857ced99d44535780a5937d
Alexwebcreator <Sanek72Tob@mail.ru>
2021-09-22 08:50:00 +0500

    create: added marker in Yandex map

--- js/main.js
+++ js/main.js
@@ -17,15 +17,26 @@ const swiper = new Swiper('.swiper', {
 
 ymaps.ready(init);
     function init(){
-        // Создание карты.
-        var myMap = new ymaps.Map("map", {
-            // Координаты центра карты.
-            // Порядок по умолчанию: «широта, долгота».
-            // Чтобы не определять координаты центра карты вручную,
-            // воспользуйтесь инструментом Определение координат.
+        let map = new ymaps.Map("map", {
             center: [7.890656, 98.294699],
-            // Уровень масштабирования. Допустимые значения:
-            // от 0 (весь мир) до 19.
             zoom: 15
         });
+        let placemark = new ymaps.Placemark([7.890656, 98.294699], {}, {
+          iconLayout: 'default#image',
+          iconImageHref: 'https://cdn-icons-png.flaticon.com/512/787/787535.png',
+          iconImageSize: [40, 40],
+          iconImageOffset: [-20, -40],
+        });
+
+        map.controls.remove('geolocationControl'); // удаляем геолокацию
+        map.controls.remove('searchControl'); // удаляем поиск
+        map.controls.remove('trafficControl'); // удаляем контроль трафика
+        map.controls.remove('typeSelector'); // удаляем тип
+        map.controls.remove('fullscreenControl'); // удаляем кнопку перехода в полноэкранный режим
+        map.controls.remove('zoomControl'); // удаляем контрол зуммирования
+        map.controls.remove('rulerControl'); // удаляем контрол правил
+        map.behaviors.disable(['scrollZoom']);
+
+        map.geoObjects.add(placemark);
     }
+   
