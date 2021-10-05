a11c6655ae5ca1f2a3fd975a3503a9341a45ecea
Alexwebcreator <Sanek72Tob@mail.ru>
2021-10-05 21:46:55 +0500

    fixed: validate form

--- js/main.js
+++ js/main.js
@@ -64,11 +64,12 @@ $(document).ready(function() {
           minlength: "Name must be at least 2 letters long",
         },
         email: {
-          required: "We need your email address to contact you",
+          required: "We need email address to contact you",
           email: "Your email address must be in the format of name@domain.com"
         },
         phone: {
           required: "Phone number is required",
+          minlength: "Phone number must contain at least 10 numbers long",
         },
       },
     });
